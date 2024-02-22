1c76be17e0222f4a4135abfef057fdfa56980c04
Екатерина <159002385+brandbook24@users.noreply.github.com>
2024-02-23 00:44:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+let rowViewButton = document.querySelector('.row-view');
+let tileViewButton = document.querySelector('.tile-view');
+let newsList = document.querySelector('.news-list');
+
+rowViewButton.onclick = function () {
+  rowViewButton.classList.add('view-checked');
+  tileViewButton.classList.remove('view-checked');
+  newsList.classList.remove('list-tiles-view');
+};
+
+tileViewButton.onclick = function () {
+  rowViewButton.classList.remove('view-checked');
+  tileViewButton.classList.add('view-checked');
+  newsList.classList.add('list-tiles-view');
+};
+
+
+
+let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function () {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+};
+
+
+let upButton = document.querySelector('.up-button');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 200) {
+    upButton.classList.add('shown');
+  } 
+  else {
+    upButton.classList.remove('shown');
+  }
+
+};
+
+upButton.onclick = function () {
+  window.scrollTo(0, 0);
+};
+
+
 
